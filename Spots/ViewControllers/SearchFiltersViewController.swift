fe444afb000bbe22f179f13554b2a85317227c0f
Alexander Grach <goodbox@Alexanders-MBP.domain>
2018-08-08 23:07:06 -0600

    start of search

---
+++ Spots/ViewControllers/SearchFiltersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchFiltersViewController.swift
+//  goodspots
+//
+//  Created by Alexander Grach on 8/3/18.
+//  Copyright © 2018 goodbox. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class SearchFiltersViewController : UIViewController {
+
+    @IBAction func didCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
